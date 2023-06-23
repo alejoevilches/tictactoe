2c0559d64fc8299ddce992f0c61f019b18f87e9f
alejoevilches <alejoevilches@gmail.com>
2023-06-23 17:11:20 -0300

    updates in game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,19 +7,50 @@ import './App.css'
 const turns={
   X:"X",
   O:"O"
+};
+
+const Square=({children, isSelected, updateBoard, index})=>{
+  const className=`square ${isSelected ? "is-selected" : ""}`
+  const handleClick=()=>{
+    const newBoard=[...board]
+    newBoard[index]=turn; 
+    setBoard(newBoard);
+    updateBoard(index); 
+  }
+  return (
+    <div onClick={handleClick } className={className}>
+      {children}
+    </div>
+  )
 }
 
-const board=Array(9).fill(null);
-
 function App() {
+  const [board, setBoard]=useState(Array(9).fill(null));
+  const [turn, setTurn]=useState(turns.X)
+  const updateBoard=()=>{
+    const newTurn = turn == turns.O ? turns.X : turns.O; 
+    setTurn(newTurn);
+  }
   return (
-  <main className="board">
-    <h1>Tic tac toe</h1>
-    <section className="game">
-      
-    </section>
-  </main>
-  )
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {board.map((cell,index)=>{
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}>
+            </Square>
+          )
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn==turns.X}>{turns.X}</Square>
+        <Square isSelected={turn==turns.O}>{turns.O}</Square>
+      </section>
+    </main>
+  );
 }
 
 export default App
