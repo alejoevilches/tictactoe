79115d86dd3a04be234d39b88426844551ba8bc0
alejoevilches <alejoevilches@gmail.com>
2023-07-04 18:04:47 -0300

    game ends when there is a winner

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,43 @@ const Square=({children, isSelected, updateBoard, index})=>{
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+const checkWinner=(boardToCheck)=>{
+  for (const combos of WINNER_COMBOS){
+    const [a,b,c]=combos;
+    if (boardToCheck[a] && boardToCheck[a]==boardToCheck[b] && boardToCheck[a]==boardToCheck[c]){
+      return boardToCheck[a];
+    }
+  }
+  return null
+}
+
 function App() {
   const [board, setBoard]=useState(Array(9).fill(null));
   const [turn, setTurn]=useState(turns.X)
+  const [winner, setWinner] = useState(null);
   const updateBoard=(index)=>{
-    if (board[index]) return;
+    if (board[index] || winner) return;
     const newTurn = turn == turns.O ? turns.X : turns.O; 
     setTurn(newTurn);
     let newBoard=[...board];
     newBoard[index]=turn;
     setBoard(newBoard);
+    const newWinner=checkWinner(newBoard);
+    if (newWinner){
+      setWinner(newWinner);
+      alert(`El ganador es ${turn}`)
+    }
   }
   return (
     <main className='board'>
